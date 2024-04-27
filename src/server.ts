887bf6ccf4ba806d85490b129f7b8d65f5eab7a2
Anirudh Jwala <anirudhsai2@gmail.com>
2024-04-27 12:06:49 +0530

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
+import { Config } from './config';
+
+console.log('PORT', Config.PORT);
+console.log('NODE_ENV', Config.NODE_ENV);
+
 function login(username: string): string {
   return username;
 }
